aaf593c90d396ee5d951f413ab044efdb4433578
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-09-17 13:03:38 +0200

    Add tests to getNarmesteLeder for unauthorized and not found

--- src/test/kotlin/no/nav/syfo/narmesteleder/NarmestelederClientTest.kt
+++ src/test/kotlin/no/nav/syfo/narmesteleder/NarmestelederClientTest.kt
@@ -1,5 +1,6 @@
 package no.nav.syfo.narmesteleder
 
+import io.ktor.client.features.ClientRequestException
 import io.ktor.http.HttpStatusCode
 import io.mockk.coEvery
 import io.mockk.mockk
@@ -8,9 +9,11 @@ import no.nav.syfo.azuread.AccessTokenClient
 import no.nav.syfo.util.HttpClientTest
 import no.nav.syfo.util.ResponseData
 import org.amshove.kluent.shouldBe
+import org.amshove.kluent.shouldEqual
 import org.amshove.kluent.shouldNotBe
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
+import kotlin.test.assertFailsWith
 
 class NarmestelederClientTest : Spek({
     val httpClientTest = HttpClientTest()
@@ -25,6 +28,22 @@ class NarmestelederClientTest : Spek({
                 val response = narmesteLederClient.getNarmesteleder("org", "aktor")
                 response shouldNotBe null
                 response.narmesteLederRelasjon shouldBe null
+            }
+        }
+
+        it("get unauthorized response") {
+            runBlocking {
+                httpClientTest.responseData = ResponseData(HttpStatusCode.Unauthorized, "Unauthorized", headersOf("Content-Type", listOf("Text")))
+                val exception = assertFailsWith<ClientRequestException> { narmesteLederClient.getNarmesteleder("org", "aktor") }
+                exception.response.status shouldEqual HttpStatusCode.Unauthorized
+            }
+        }
+
+        it("get not found response") {
+            runBlocking {
+                httpClientTest.responseData = ResponseData(HttpStatusCode.NotFound, "Not found", headersOf("Content-Type", listOf("Text")))
+                val exception = assertFailsWith<ClientRequestException> { narmesteLederClient.getNarmesteleder("org", "aktor") }
+                exception.response.status shouldEqual HttpStatusCode.NotFound
             }
         }
     }
