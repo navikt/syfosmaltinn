4a8c4eac3ebdd1e8a920791eb10ebd2c95039f80
andreasnilsen <danduras@gmail.com>
2020-09-21 16:16:19 +0200

    more logging from narmeste leder

--- src/main/kotlin/no/nav/syfo/narmesteleder/NarmestelederClient.kt
+++ src/main/kotlin/no/nav/syfo/narmesteleder/NarmestelederClient.kt
@@ -1,23 +1,31 @@
 package no.nav.syfo.narmesteleder
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.receive
 import io.ktor.client.request.accept
 import io.ktor.client.request.get
 import io.ktor.client.request.headers
+import io.ktor.client.statement.HttpStatement
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import no.nav.syfo.azuread.AccessTokenClient
+import no.nav.syfo.log
 
 class NarmestelederClient(private val httpClient: HttpClient, private val accessTokenClient: AccessTokenClient, private val baseUrl: String) {
 
     suspend fun getNarmesteleder(orgnummer: String, aktorId: String): NarmestelederResponse {
         val token = accessTokenClient.getAccessToken()
-        return httpClient.get("$baseUrl$NARMESTE_LEDER_URL/$aktorId?orgnummer=$orgnummer") {
+        log.info("got token from azuread")
+        val statement = httpClient.get<HttpStatement>("$baseUrl$NARMESTE_LEDER_URL/$aktorId?orgnummer=$orgnummer") {
             headers {
                 append(HttpHeaders.Authorization, "Bearer $token")
             }
             accept(ContentType.Application.Json)
-        }
+        }.execute()
+        val status = statement.status
+        log.info("Got status $status from NarmesteLeder")
+        return statement.receive()
+
     }
 
     companion object {
