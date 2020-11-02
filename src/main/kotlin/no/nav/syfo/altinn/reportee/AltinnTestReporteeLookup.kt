3d3867f1bab31232e32958a0bed4424f4075f7be
andreasnilsen <danduras@gmail.com>
2020-11-02 08:39:39 +0100

    Run kafkaconsumer and add testorgn as resources

--- src/main/kotlin/no/nav/syfo/altinn/reportee/AltinnTestReporteeLookup.kt
+++ src/main/kotlin/no/nav/syfo/altinn/reportee/AltinnTestReporteeLookup.kt
@@ -1,10 +1,8 @@
 package no.nav.syfo.altinn.reportee
 
-import no.nav.syfo.getFileAsString
-
 class AltinnTestReporteeLookup : AltinnReporteeLookup {
-    private val overrideReportee: String = getFileAsString("/secrets/vault/altinn_test_overstyr_orgnr")
-    private val altinnWhitelistReportee: List<String> = getFileAsString("/secrets/vault/altinn_test_whitelist_orgnr").split(",")
+    private val overrideReportee: String = AltinnTestReporteeLookup::class.java.getResource("/altinn/altinn_test_overstyr_orgnr").readText().trim()
+    private val altinnWhitelistReportee: List<String> = AltinnTestReporteeLookup::class.java.getResource("/altinn/altinn_test_whitelist_orgnr").readText().trim().split(",")
 
     override fun getReportee(orgnummer: String): String {
         return when (altinnWhitelistReportee.contains(orgnummer)) {
--- src/test/kotlin/no/nav/syfo/altinn/util/AltinnReporteeResolverTest.kt
+++ src/test/kotlin/no/nav/syfo/altinn/util/AltinnReporteeResolverTest.kt
@@ -1,21 +1,13 @@
 package no.nav.syfo.altinn.util
 
-import io.mockk.every
-import io.mockk.mockkStatic
 import no.nav.syfo.altinn.reportee.AltinnReporteeLookupFacotry
-import no.nav.syfo.getFileAsString
 import org.amshove.kluent.shouldEqual
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
 class AltinnReporteeResolverTest : Spek({
 
-    val testOverride = "testorg"
-    val testWithlisted = "whitelist1,whitelist2,whitelist3"
-
-    mockkStatic("no.nav.syfo.EnvironmentKt")
-    every { getFileAsString("/secrets/vault/altinn_test_overstyr_orgnr") } returns testOverride
-    every { getFileAsString("/secrets/vault/altinn_test_whitelist_orgnr") } returns testWithlisted
+    val testOverride = "910067494"
 
     describe("Test ReporteeResolvers") {
         it("Shuild get testOverride") {
@@ -23,9 +15,9 @@ class AltinnReporteeResolverTest : Spek({
             testOverride shouldEqual AltinnReporteeLookupFacotry.getReporteeResolver("dev-fss").getReportee("whitelist4")
         }
         it("should get whitelisted") {
-            "whitelist1" shouldEqual AltinnReporteeLookupFacotry.getReporteeResolver("dev-fss").getReportee("whitelist1")
-            "whitelist2" shouldEqual AltinnReporteeLookupFacotry.getReporteeResolver("dev-fss").getReportee("whitelist2")
-            "whitelist3" shouldEqual AltinnReporteeLookupFacotry.getReporteeResolver("dev-fss").getReportee("whitelist3")
+            "811290572" shouldEqual AltinnReporteeLookupFacotry.getReporteeResolver("dev-fss").getReportee("811290572")
+            "811290742" shouldEqual AltinnReporteeLookupFacotry.getReporteeResolver("dev-fss").getReportee("811290742")
+            "910975439" shouldEqual AltinnReporteeLookupFacotry.getReporteeResolver("dev-fss").getReportee("910975439")
         }
         it("Should get same orgnummer in prod") {
             "1" shouldEqual AltinnReporteeLookupFacotry.getReporteeResolver("prod-fss").getReportee("1")
