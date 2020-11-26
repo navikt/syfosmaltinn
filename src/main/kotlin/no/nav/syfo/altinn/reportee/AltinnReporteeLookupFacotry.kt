010398ca999c1ba495748ad0a25b139131667983
andreasnilsen <danduras@gmail.com>
2020-11-26 15:19:49 +0100

    fix compile error

--- src/main/kotlin/no/nav/syfo/altinn/reportee/AltinnReporteeLookupFacotry.kt
+++ src/main/kotlin/no/nav/syfo/altinn/reportee/AltinnReporteeLookupFacotry.kt
@@ -3,7 +3,7 @@ package no.nav.syfo.altinn.reportee
 class AltinnReporteeLookupFacotry private constructor () {
     companion object {
 
-        private const val DEV_CLUSTER = listOf("dev-fss", "dev-gcp")
+        private val DEV_CLUSTER = listOf("dev-fss", "dev-gcp")
 
         fun getReporteeResolver(cluster: String): AltinnReporteeLookup {
             return when (DEV_CLUSTER.contains(cluster)) {
