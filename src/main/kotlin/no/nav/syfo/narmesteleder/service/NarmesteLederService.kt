09be671f1d348663d1bb031eb26e52bed5078c9a
andreasnilsen <danduras@gmail.com>
2021-10-29 15:00:52 +0200

    Remove rest endpoint

--- src/main/kotlin/no/nav/syfo/narmesteleder/service/NarmesteLederService.kt
+++ src/main/kotlin/no/nav/syfo/narmesteleder/service/NarmesteLederService.kt
@@ -2,24 +2,24 @@ package no.nav.syfo.narmesteleder.service
 
 import io.ktor.util.KtorExperimentalAPI
 import no.nav.syfo.client.StsOidcClient
-import no.nav.syfo.narmesteleder.client.NarmestelederClient
+import no.nav.syfo.narmesteleder.db.NarmestelederDB
 import no.nav.syfo.narmesteleder.model.NarmesteLeder
 import no.nav.syfo.pdl.client.PdlClient
 import no.nav.syfo.pdl.client.model.Person
 
 @KtorExperimentalAPI
 class NarmesteLederService(
-    private val narmestelederClient: NarmestelederClient,
+    private val narmestelederDB: NarmestelederDB,
     private val pdlClient: PdlClient,
     private val stsOidcClient: StsOidcClient
 ) {
     suspend fun getNarmesteLeder(orgnummer: String, fnr: String): NarmesteLeder? {
-        return narmestelederClient.getNarmesteleder(
-            orgnummer,
-            fnr
-        ).narmesteLederRelasjon?.let { narmesteLederRelasjon ->
+        return narmestelederDB.getNarmesteleder(
+            sykmeldtFnr = fnr,
+            orgnummer = orgnummer
+        )?.let { narmesteLederRelasjon ->
 
-            val lederPerson = pdlClient.getPerson(narmesteLederRelasjon.narmesteLederFnr, stsOidcClient.oidcToken().access_token)
+            val lederPerson = pdlClient.getPerson(narmesteLederRelasjon.lederFnr, stsOidcClient.oidcToken().access_token)
 
             NarmesteLeder(
                 epost = narmesteLederRelasjon.narmesteLederEpost,
