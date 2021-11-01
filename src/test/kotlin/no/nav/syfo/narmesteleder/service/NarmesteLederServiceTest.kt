45413b8957ff4abe2071c9ae9849c4250602cd2e
andreasnilsen <danduras@gmail.com>
2021-11-01 11:18:08 +0100

    fix test

--- src/test/kotlin/no/nav/syfo/narmesteleder/service/NarmesteLederServiceTest.kt
+++ src/test/kotlin/no/nav/syfo/narmesteleder/service/NarmesteLederServiceTest.kt
@@ -5,6 +5,7 @@ import io.mockk.clearAllMocks
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
+import java.time.LocalDate
 import kotlinx.coroutines.runBlocking
 import no.nav.syfo.client.StsOidcClient
 import no.nav.syfo.narmesteleder.db.NarmestelederDB
@@ -15,7 +16,6 @@ import no.nav.syfo.pdl.client.model.Person
 import org.amshove.kluent.shouldEqual
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
-import java.time.LocalDate
 
 @KtorExperimentalAPI
 object NarmesteLederServiceTest : Spek({
@@ -31,28 +31,28 @@ object NarmesteLederServiceTest : Spek({
     val leder = Person("Fornavn", "Mellomnavn", "Etternavn", "aktørid", "lederFnr")
     describe("NarmestelederService - leder får riktig navn") {
 
-        it ("Should get correct NL") {
+        it("Should get correct NL") {
             every { narmestelederDb.getNarmesteleder("1", "orgnummer") } returns NarmestelederDbModel(
                 sykmeldtFnr = "1",
                 lederFnr = "lederFnr",
                 orgnummer = "orgnummer",
                 narmesteLederTelefonnummer = "telefon",
                 narmesteLederEpost = "epost",
-                aktivFom = LocalDate.of(2021, 1,1),
+                aktivFom = LocalDate.of(2021, 1, 1),
                 arbeidsgiverForskutterer = true
             )
 
             coEvery { pdlClient.getPerson("lederFnr", "access_token") } returns leder
 
-            val nl = runBlocking { narmesteLederService.getNarmesteLeder( orgnummer = "orgnummer",fnr = "1") }
+            val nl = runBlocking { narmesteLederService.getNarmesteLeder(orgnummer = "orgnummer", fnr = "1") }
             nl shouldEqual NarmesteLeder("epost", "orgnummer", "telefon", LocalDate.of(2021, 1, 1), true, "Fornavn Mellomnavn Etternavn", "lederFnr")
         }
 
-        it ("Should get null") {
+        it("Should get null") {
             every { narmestelederDb.getNarmesteleder("1", "orgnummer") } returns null
             coEvery { pdlClient.getPerson("lederFnr", "access_token") } returns leder
             val nl = runBlocking { narmesteLederService.getNarmesteLeder("orgnummer", "1") }
-            nl shouldEqual NarmesteLeder("epost", "orgnummer", "telefon", LocalDate.of(2021, 1, 1), true, "Fornavn Mellomnavn Etternavn", "lederFnr")
+            nl shouldEqual null
         }
         it("Leder med mellomnavn") {
             val navn = narmesteLederService.getName(leder)
