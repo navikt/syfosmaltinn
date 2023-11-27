51ade771bb294789fae7a6179a1f360ffde6a099
Helene Arnesen <helene.arnesen@nav.no>
2023-11-27 12:33:17 +0100

    add flyway-database and bump flyway version

--- build.gradle.kts
+++ build.gradle.kts
@@ -105,6 +105,7 @@ dependencies {
     implementation("org.postgresql:postgresql:$postgresVersion")
     implementation("com.zaxxer:HikariCP:$hikariVersion")
     implementation("org.flywaydb:flyway-core:$flywayVersion")
+    implementation("org.flywaydb:flyway-database-postgresql:$flywayVersion")
 
     implementation("com.google.cloud:google-cloud-storage:$googleCloudStorageVersion")
 
